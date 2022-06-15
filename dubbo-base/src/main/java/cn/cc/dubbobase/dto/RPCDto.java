9026ec76e749188002fbf511a6df1f3a7738ea37
everforcc <718497737@qq.com>
2022-06-15 23:10:59 +0800

    20220615 管理包版本,maven打包管理

---
+++ dubbo-base/src/main/java/cn/cc/dubbobase/dto/RPCDto.java
@@ -0,0 +1,36 @@
+/**
+ * Project:TODO ADD PROJECT NAME SpringIODubbo
+ *
+ * @Description
+ * @Author Author Date Description
+ * ------ ------ ------
+ *    TODO 开发人员邮箱前缀 调整时间 年-月-日 主要改动点>5字符
+ * @Date 2022-06-15 22:20
+ * Copyright
+ */
+
+package cn.cc.dubbobase.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RPCDto {
+
+    private long id;
+
+    private String str;
+
+    private Date date;
+
+    private List<String> stringList;
+
+}
