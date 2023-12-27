885003d744d306b84035c8d51c09a9718a3b14e3
everforcc <48779058+everforcc@users.noreply.github.com>
2023-12-27 23:06:19 +0800

    20231227 测试类示例

--- apache-2.7-provider/src/test/java/cn/cc/apache_dubbo_2_7/Apache27ProviderApplicationTests.java
+++ apache-2.7-provider/src/test/java/cn/cc/apache_dubbo_2_7/Apache27ProviderApplicationTests.java
@@ -1,9 +1,12 @@
 package cn.cc.apache_dubbo_2_7;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Apache_27_ProviderApplication.class})
 class Apache27ProviderApplicationTests {
 
     @Test
